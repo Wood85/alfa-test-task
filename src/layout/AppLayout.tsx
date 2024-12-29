0bb11d95b1d31294fb4a9711c18ed857de2a8779
Aleksandr Krivoshein <megalistnn@gmail.com>
2024-12-29 20:48:21 +0400

    feat: add footer

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+
+import styles from "./AppLayout.module.css";
 
 export function AppLayout() {
   return (
-    <main>
-      <header>Header</header>
-        <Outlet />
-			<footer>Footer</footer>
+    <main className={styles.page}>
+      <div className={styles.header}>
+				Header
+			</div>
+			<div className={styles.outlet}>
+			  <Outlet />
+			</div>
+			<div className={styles.footer}>
+				<Footer/>
+			</div>
     </main>
   );
 }
